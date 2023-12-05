6b7073d5d9fefcaab69ee602650cfe02652e9be0
sangyeol9 <xhdfud1@naver.com>
2023-12-05 17:12:07 +0900

    rpg 수정

--- src/com/sangs/s01/Study04For.java
+++ src/com/sangs/s01/Study04For.java
@@ -29,14 +29,15 @@ public class Study04For {
 		int gold = 0;
 		int contents = 1;
 		int exp;
-		
+		int maxlevel = 15;
+		boolean login = false;
 		while(coin==0) {
 			System.out.println("로그인 : 0 , 종료는 1번");
 			coin = sc.nextInt();
 			if(coin==1) {
 				System.out.println("게임을 종료합니다.");
 				break;
-			}
+			}else if(coin==0) {
 			System.out.println("ID를 입력하세요.");
 			ID = sc.nextInt();
 			System.out.println("PW를 입력하세요.");
@@ -44,9 +45,15 @@ public class Study04For {
 			if(ID==idche && PW==pwche) {
 				coin = 2;
 				System.out.println("로그인에 성공하였습니다!!");
+				login=true;
+			}
+			}
+			else {
+				System.out.println("다시 입력해");
+				coin=0;
 				
 			}
-		if(coin==2) {
+		if(login) {
 			System.out.println("게임에 접속하였습니다.");
 			System.out.println("당신의 레벨은 " + level +"입니다.");
 			exp = level*3;
@@ -81,19 +88,26 @@ public class Study04For {
 			exp = level *3;
 			if(level == 15) {
 				System.out.println("최고레벨달성!!!최고레벨달성!!!최고레벨달성!!!최고레벨달성!!!최고레벨달성!!!최고레벨달성!!!최고레벨달성!!!최고레벨달성!!!최고레벨달성!!!최고레벨달성!!!최고레벨달성!!!최고레벨달성!!!최고레벨달성!!!최고레벨달성!!!");
+				while(maxlevel<=15) {
+				System.out.println("max레벨을 입력해주세요");
+				maxlevel = sc.nextInt();
+				}
 				while(contents==1) {
-					System.out.println("더 이상 레벨이 오르지않습니다.");
+					System.out.println("추가적으로 레벨을 올리시겠습니까?");
 					System.out.println("사냥 : 1   종료 : 2");
 					contents = sc.nextInt();
 					if(contents==1) {
 					for(int i=1;i<=exp;i++) {
 						System.out.println(i + "마리 잡았습니다.");
 					}
-					System.out.println("최대레벨 도달했으므로 레벨업 하지않습니다.");
+					level++;
+					exp =level*3;
+					System.out.println("레벨업! 현재레벨은 " + level);
 					}
 					else if(contents==2) { System.out.println("게임을 종료합니다. 안녕히가세요");
 					System.out.println("현재 레벨은 " + level + " 현재 보유 골드 : " + gold);
 					}
+					if(maxlevel==level) contents = 3;
 			}
 			}
 		}
@@ -112,3 +126,11 @@ public class Study04For {
 		
 	}
 }
+
+
+
+
+
+
+
+//
