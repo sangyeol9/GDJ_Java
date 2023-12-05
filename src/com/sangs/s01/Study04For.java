8b19f1dd2f892ce9bacf2590222c8b387a29a483
sangyeol9 <xhdfud1@naver.com>
2023-12-05 15:16:00 +0900

    s4

--- src/com/sangs/s01/Study04For.java
+++ src/com/sangs/s01/Study04For.java
@@ -8,18 +8,24 @@ public class Study04For {
 		//30 * 3 / 1. 단발 , 2점사 
 		
 		Scanner sc = new Scanner(System.in);
-		
-		
+		int k = 0;
+		int shot =0;
 		for(int i=1;i<=3;i++) {
+			k=0;
+			while(k==0) {
 			System.out.println("사격 방식을 선택해주세요. 1:단발 2:점사");
-			int shot = sc.nextInt();
+			 shot = sc.nextInt();
+			if(shot==1 || shot ==2) {
+				k=1;
+			}
+			}
 			for(int j=1;j<=30;j++) {
 				if(shot == 1 ) {
 					System.out.println("탕");
 				}
 				else if(shot == 2) {
 					System.out.println("탕탕탕");
-					j = j+2;
+					j = j+2;     
 				}
 			}
 			
